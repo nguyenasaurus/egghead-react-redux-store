22fac5dae64000752d4f7bf105eb203e54bae638
Justine Nguyen <justine@exordo.com>
2023-06-23 11:08:56 +0100

    feat: update cart number items

--- src/features/cart/CartLink.tsx
+++ src/features/cart/CartLink.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks";
+import { getNumItems } from "./cartSlice";
 import styles from "./CartLink.module.css";
 
 export function CartLink() {
+  const numItems = useAppSelector(getNumItems);
   return (
     <Link to="/cart" className={styles.link}>
-      <span className={styles.text}>🛒&nbsp;&nbsp;Cart</span>
+      <span className={styles.text}>🛒&nbsp;&nbsp;{numItems ||"Cart"}</span>
     </Link>
   );
 }
