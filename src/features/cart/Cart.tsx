dbd013285d3847c37f229c94114eece802c40799
Justine Nguyen <justine@exordo.com>
2023-06-23 13:51:48 +0100

    feat: calculate cart total

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useAppSelector } from "../../app/hooks";
+import { getTotalPrice } from "./cartSlice";
 import styles from "./Cart.module.css";
 
 export function Cart() {
   const products = useAppSelector(state => state.products.products);
-  const items = useAppSelector(state => state.cart.items)
+  const items = useAppSelector(state => state.cart.items);
+  const totalPrice = useAppSelector(getTotalPrice);
+
   return (
     <main className="page">
       <h1>Shopping Cart</h1>
@@ -37,7 +40,7 @@ export function Cart() {
           <tr>
             <td>Total</td>
             <td></td>
-            <td className={styles.total}>${0.0}</td>
+            <td className={styles.total}>${totalPrice}</td>
             <td></td>
           </tr>
         </tfoot>
--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -45,3 +45,15 @@ export const getMemoizedItems = createSelector(
     return numItems;
   }
 )
+
+export const getTotalPrice = createSelector(
+  (state: RootState) => state.cart.items,
+  (state: RootState) => state.products.products,
+  (items, products) => {
+    let total = 0;
+    for (let id in items) {
+      total += products[id].price * items[id];
+    }
+    return total.toFixed(2);
+  }
+)
